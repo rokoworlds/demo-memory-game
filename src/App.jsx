46fa4185fe0b1a96d6883f6e320aa24fbc19e0d0
Roman Coropat <roman.koropat@gmail.com>
2025-03-17 20:34:23 +0200

    add: random fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,46 @@ import MemoryCard from "./components/MemoryCard";
 
 function App() {
 const [isGameOn, setIsGameOn] = useState(false);
+const [emojiData, setEmojiData] = useState([]);
 
 async function startGame(e) {
   e.preventDefault();
   try {
     const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature");
     if (!response.ok) {
-      console.log(response.text)
-      throw new Error(`Response status: ${response.status}`);
+      throw new Error('Could not fetch data from API');
     } 
     
     const data = await response.json();
-    console.log(data);
+    const dataSample = getDataSlice(data);
+    setEmojiData(dataSample);
     setIsGameOn(true);
 
   } catch (error) {
     console.error(error.message);
   }
+}
 
+function getDataSlice(data) {
+  const randomIndices = getRandomIndices(data);
+
+  return randomIndices.map(index => data[index])
+}
+
+function getRandomIndices(data) {
+  const max = data.length;
+  const randomIndicesArray = [];
+
+  for (let i = 0; i < 5; i++) {
+    const newId = Math.floor(Math.random() * max);
+    if (!(newId in randomIndicesArray)) {
+      randomIndicesArray.push(newId)
+    } else {
+      i--;
+    }
+  }
+
+  return randomIndicesArray;
 }
 
 function turnCard(){
@@ -34,10 +56,20 @@ function turnCard(){
       <main>
         <h1>Memory Game</h1>
         {!isGameOn && <Form handleSubmit={startGame} />}
-        {isGameOn && <MemoryCard handleClick={turnCard} />}
+        {isGameOn && <MemoryCard data={emojiData} handleClick={turnCard} />}
       </main>
     </>
   )
 }
 
 export default App
+
+    /**
+     * Challenge:
+
+     * 3) Map over "randomIndices" and use the random numbers stored in this array to create a new array of random emojis selected from "data". Store this new array in a variable called "dataSlice" and return it at the bottom of the function.
+     * 4) Inside the try block of the "startGame" function, make a call to "getDataSlice", passing "data" as an argument. Save the return value in a variable called "dataSlice".
+     * 5) Delete the "dataSample" variable and replace "dataSample" with the new "dataSlice" variable in the "setEmojisData" function.
+     * 6) Run the code and start a new game to check that your code is working.
+    */  
+    
