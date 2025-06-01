d19efb9b7b7a86b8be69af00afb282c79b979da1
Roman Coropat <roman.koropat@gmail.com>
2025-06-01 23:01:55 +0300

    debug: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,100 +11,101 @@ function App() {
 
   const [formData, setFormData] = useState(initialFormData)
 
-const [isGameOn, setIsGameOn] = useState(false);
-const [emojiData, setEmojiData] = useState([]);
-const [selectedCards, setSelectedCards] = useState([]);
-const [matchedCards, setMatcedCards] = useState([]);
-const [areAllCardsMatched, setAreAllCardsMatched] = useState(false);
-const [isError, setIsError] = useState(false)
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [emojiData, setEmojiData] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [matchedCards, setMatcedCards] = useState([]);
+  const [areAllCardsMatched, setAreAllCardsMatched] = useState(false);
+  const [isError, setIsError] = useState(false)
+  const [isFirstRender, setIsFirstRender] = useState(true)
 
-useEffect(() => {
-  if (emojiData.length && matchedCards.length === emojiData.length) {
-    setAreAllCardsMatched(true);
-    console.log(`GAME OVER: ${areAllCardsMatched}`)
+  useEffect(() => {
+    if (emojiData.length && matchedCards.length === emojiData.length) {
+      setAreAllCardsMatched(true);
+      console.log(`GAME OVER: ${areAllCardsMatched}`)
+    }
+  }, [matchedCards])
+
+
+  useEffect(() => {
+    if (selectedCards.length === 2 && selectedCards[0].name === selectedCards[1].name) {
+      setMatcedCards(prev => [...prev, ...selectedCards])
+    }
+  }, [selectedCards])
+
+  function handleFormChange(e) {
+    setFormData(prev => ({...prev, [e.target.name] : e.target.value}))
   }
-}, [matchedCards])
 
+  async function startGame(e) {
+    e.preventDefault();
 
-useEffect(() => {
-  if (selectedCards.length === 2 && selectedCards[0].name === selectedCards[1].name) {
-    setMatcedCards(prev => [...prev, ...selectedCards])
-  }
-}, [selectedCards])
-
-function handleFormChange(e) {
-  setFormData(prev => ({...prev, [e.target.name] : e.target.value}))
-}
-
-async function startGame(e) {
-  e.preventDefault();
-
-  try {
-    const response = await fetch(`https://emojihub.yurace.pro/api/all/category/${formData.category}`);
-    
-    if (!response.ok) {
-      throw new Error('Could not fetch data from API');
-    } 
-    
-    const data = await response.json();
-    const dataSlice = getDataSlice(data);
-    const emojisArray = getEmojisArray(dataSlice)
-    
-    setEmojiData(emojisArray);
-    setIsGameOn(true);
-
-  } catch (error) {
-    setIsError(true)
-    console.error(error.message);
-  }
-}
-
-function getDataSlice(data) {
-  const randomIndices = getRandomIndices(data);
-
-  return randomIndices.map(index => data[index])
-}
-
-function getRandomIndices(data) {
-  const max = data.length;
-  const randomIndicesArray = [];
-
-  for (let i = 0; i < formData.number / 2; i++) {
-    const newId = Math.floor(Math.random() * max);
-    if (!(newId in randomIndicesArray)) {
-      randomIndicesArray.push(newId)
-    } else {
-      i--;
+    try {
+      const response = await fetch(`https://emojihub.yurace.pro/api/all/category/${formData.category}`);
+      
+      if (!response.ok) {
+        throw new Error('Could not fetch data from API');
+      } 
+      
+      const data = await response.json();
+      const dataSlice = getDataSlice(data);
+      const emojisArray = getEmojisArray(dataSlice)
+      
+      setEmojiData(emojisArray);
+      setIsGameOn(true);
+    } catch (error) {
+        console.log(error)
+        setIsError(true)
+    } finally {
+        setIsFirstRender(false)
     }
   }
 
-  return randomIndicesArray;
+  function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+
+    return randomIndices.map(index => data[index])
+  }
+  
+  function getRandomIndices(data) {        
+    const randomIndicesArray = []
+
+    for (let i = 0; i < (formData.number / 2); i++) {
+        const randomNum = Math.floor(Math.random() * data.length)
+        if (!randomIndicesArray.includes(randomNum)) {
+            randomIndicesArray.push(randomNum)
+        } else {
+            i--
+        }
+    }
+    
+    return randomIndicesArray
 }
 
-function getEmojisArray(data) {
-  const pairedEmojisArray = [...data, ...data];
+  function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
 
-  for (let i = pairedEmojisArray.length -1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i+1));
-    let k = pairedEmojisArray[i];
-    pairedEmojisArray[i] = pairedEmojisArray[j];
-    pairedEmojisArray[j] = k;
+    for (let i = pairedEmojisArray.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+      let k = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = k;
+    }
+
+    return pairedEmojisArray
   }
 
-  return pairedEmojisArray
-}
 
-
-function turnCard(name, index){
-  if (selectedCards.length < 2) {
-    setSelectedCards(prev => [...prev, {name, index}]);
-  } else if (selectedCards.length === 2) {
-    setSelectedCards([{name, index}]);
+  function turnCard(name, index){
+    if (selectedCards.length < 2) {
+      setSelectedCards(prev => [...prev, {name, index}]);
+    } else if (selectedCards.length === 2) {
+      setSelectedCards([{name, index}]);
+    }
   }
-}
 
   function resetGame() {
-    setIsGameOn(true);
+    setIsGameOn(false);
     setSelectedCards([])
     setMatcedCards([])
     setAreAllCardsMatched(false)
@@ -119,7 +120,12 @@ function turnCard(name, index){
     <>
       <main>
         <h1>Memory Game</h1>
-        {!isGameOn && !isError && <Form handleSubmit={startGame} handleChange={handleFormChange} />}
+        {!isGameOn && !isError && 
+          <Form 
+            handleSubmit={startGame} 
+            handleChange={handleFormChange}
+            isFirstRender={isFirstRender}
+          />}
 
         {isGameOn && !areAllCardsMatched && 
           <AssistiveTechInfo 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,38 +1,21 @@
-import RegularButton from "./RegularButton";
+import { useRef, useEffect } from 'react'
+import RegularButton from './RegularButton'
+import Select from './Select'
 
-export default function Form({handleSubmit, handleChange}) {
+export default function Form({ handleSubmit, handleChange, isFirstRender }) {
+    const divRef = useRef(null)
+    
+    useEffect(() => {
+        !isFirstRender && divRef.current.focus()
+    }, [])
+    
     return (
-        <div className="form-container">
+        <div className="form-container" ref={divRef} tabIndex={-1}>
+            <p className="p--regular">
+                Customize the game by selecting an emoji category and a number of memory cards.
+            </p>
             <form className="wrapper">
-                <div className="form__inner-wrapper">
-                    <label htmlFor="category">Choose category</label>
-                    <select 
-                        name="category" 
-                        id="category"
-                        onChange={handleChange}
-                        >
-                        <option value="animals-and-nature">Animals and Nature</option>
-                        <option value="food-and-drink">Food-and Drink</option>
-                        <option value="travel-and-places">Travel and Places</option>
-                        <option value="objects">Objects</option>
-                        <option value="symbols">Symbols</option>
-                    </select>
-
-                </div>
-                <div className="form__inner-wrapper">
-                    <label htmlFor="number">Choose number of cards</label>
-                    <select 
-                        name="number"
-                        id="number"
-                        onChange={handleChange}
-                        >
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="30">30</option>
-                        <option value="40">40</option>
-                        <option value="50">50</option>
-                    </select>
-                </div>
+                <Select handleChange={handleChange} />
                 <RegularButton handleClick={handleSubmit}>
                     Start Game
                 </RegularButton>
